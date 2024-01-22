fc44f7a52a5fb03860ce05b64d6c24e19de6f3a7
tsun99 <tsungaila99@gmail.com>
2024-01-22 15:13:55 +0200

    Basic daily update tests

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,36 @@
 import { Item, GildedRose } from '@/gilded-rose';
 
 describe('Gilded Rose', () => {
-  it('should foo', () => {
+  it('should create foo', () => {
     const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe('foo');
+  });
+});
+
+describe('Daily updates', () => {
+  it('lower sellIn and quality on update', () => {
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 7, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(6);
+    expect(items[0].sellIn).toBe(6);
+  });
+  it('quality degrades twice as fast after sellIn date passes', () => {
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', -1, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+    expect(items[0].sellIn).toBe(-2);
+  });
+  it('quality degrades twice as sellIn date reaches 0', () => {
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 0, 7)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(5);
+    expect(items[0].sellIn).toBe(-1);
+  });
+  it('quality is never negative', () => {
+    const gildedRose = new GildedRose([new Item('Elixir of the Mongoose', 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).toBe(0);
+    expect(items[0].sellIn).toBe(-1);
   });
 });
