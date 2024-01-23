87634899af5de41da919d90a672dfb340f1b3bbc
tsun99 <tsungaila99@gmail.com>
2024-01-23 19:01:33 +0200

    Logging yes count into file

--- app/NodeTask/task.ts
+++ app/NodeTask/task.ts
@@ -1,38 +1,52 @@
 import { Item } from "../Item";
 import { GildedRose } from "../gilded-rose";
-import { makeApiRequest } from './api';
+import { makeApiRequest } from "./api";
+import * as fs from "fs/promises";
 
 async function main() {
-    const updates = parseInt(process.argv[2]);
-    const requestsToApi = parseInt(process.argv[3]);
+  // receiving the input from the console
+  const updates = parseInt(process.argv[2]);
+  const requestsToApi = parseInt(process.argv[3]);
 
-    const items = [
-        new Item("+5 Dexterity Vest", 10, 20), //
-        new Item("Aged Brie", 2, 0), //
-        new Item("Elixir of the Mongoose", 5, 7), //
-        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-        new Item("Conjured Mana Cake", 3, 6)];
-      
-      const gildedRose = new GildedRose(items);
+  // initializing the shop with items
+  const items = [
+    new Item("+5 Dexterity Vest", 10, 20), //
+    new Item("Aged Brie", 2, 0), //
+    new Item("Elixir of the Mongoose", 5, 7), //
+    new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    new Item("Conjured Mana Cake", 3, 6),
+  ];
 
-      for (let i = 0; i < updates; i++) {
-        let yesResponseNumber = requestsToApi;
-        while (yesResponseNumber > 0) {
-          const apiResponses = await Promise.all(Array.from({length: yesResponseNumber}, makeApiRequest));
-          yesResponseNumber = apiResponses.filter(response => response === true).length;
-          
-          
-          //TODO: Response to log.txt
-          console.log(apiResponses);
-          console.log(yesResponseNumber);
-        }
-        break;
-        gildedRose.updateQuality();
-      }
+  const gildedRose = new GildedRose(items);
+
+  // looping through to update the shop and make api requests
+  for (let i = 0; i < updates; i++) {
+    let yesResponseCount = requestsToApi;
+
+    while (yesResponseCount > 0) {
+      // Promise.all returns a single boolean[] promise. If any of the requests fail, rejects.
+      const apiResponses = await Promise.all(
+        Array.from({ length: yesResponseCount }, makeApiRequest)
+      );
+      // updating how many 'yes' responses were received
+      yesResponseCount = apiResponses.filter(
+        (response) => response === true
+      ).length;
+
+      await fs.appendFile(
+        "log.txt",
+        `Update ${i + 1}: Yes count: ${yesResponseCount}\n`
+      );
+    }
+
+    gildedRose.updateQuality();
+
+    // console.log(gildedRose.items[1]);
+  }
 }
 
 main();
