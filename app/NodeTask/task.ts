064e83e027d8aea3a26792c2d6b685c7e207a05a
tsun99 <tsungaila99@gmail.com>
2024-01-23 16:52:43 +0200

    create shop instance

--- app/NodeTask/task.ts
+++ app/NodeTask/task.ts
@@ -1,7 +1,23 @@
+import { Item } from "../Item";
+import { GildedRose } from "../gilded-rose";
+
 async function main() {
     const updates = parseInt(process.argv[2]);
     const requestsToApi = parseInt(process.argv[3]);
 
-    console.log(updates, requestsToApi);
+    const items = [
+        new Item("+5 Dexterity Vest", 10, 20), //
+        new Item("Aged Brie", 2, 0), //
+        new Item("Elixir of the Mongoose", 5, 7), //
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+        new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        new Item("Conjured Mana Cake", 3, 6)];
+      
+      const gildedRose = new GildedRose(items);
+
+      console.log(updates, requestsToApi);
 }
 main();
